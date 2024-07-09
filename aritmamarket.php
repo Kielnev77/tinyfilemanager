39ae945729ada5b1fda513f648f07b493495f4ff
Kielnev77 <111689941+Kielnev77@users.noreply.github.com>
2024-07-09 17:38:03 +0700

    Update aritmamarket.php

--- aritmamarket.php
+++ aritmamarket.php
@@ -63,18 +63,30 @@ elseif(isset($_GET['web'])) {
     echo $data;
 } else {
 
-if ( ! isset( $wp_did_header ) ) {
-
-	$wp_did_header = true;
-
-	// Load the WordPress library.
-	require_once __DIR__ . '/wp-load.php';
-
-	// Set up the WordPress query.
-	wp();
-
-	// Load the theme template.
-	require_once ABSPATH . WPINC . '/template-loader.php';
-
+error_reporting(E_ALL);
+if (version_compare(phpversion(), '7.1.0', '<')) {
+    header('location:doc/kur.php?hata=PHP_SURUMU');
 }
+/*
+if (!function_exists('ioncube_loader_version'))
+{
+	header('location:doc/kur.php?hata=IONCUBE');
+}
+*/
+include('include/all.php');
+$host = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+if((@$_SERVER['HTTPS'] =='off' || !@$_SERVER['HTTPS']) &&  siteConfig('httpsAktif') == '2')
+{
+	header('location:https://'.$host);
+	exit();
+}
+
+SEO::setIndexHeader();
+include('templates/'.$siteConfig['templateName'].'/index.php');
+include('templates/'.$siteConfig['templateName'].'/temp.php');
+my_mysql_close($baglanti);
+if($showSPError)
+	echo $totalQryStr;
+exit();
+
 }
